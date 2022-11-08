128224f0c339cfa73f10375e3a7c6505a21a1a70
Servi <servihuijbregts@gmail.com>
2022-11-08 17:02:40 +0100

    Initial Board Push

---
+++ src/Components/BoardComponent.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./css/BoardComponent.css";
+
+export default function BoardComponent() {
+    return(
+        <div className={"board-div"}>
+            <h1>Hoi</h1>
+        </div>
+    )
+}
