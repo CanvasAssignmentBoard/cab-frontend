88fafcb048dbc11139632f272d555c77f83dfd13
Overeem,Noah N.V <n.overeem@student.fontys.nl>
2022-12-07 20:16:26 +0100

    create assignment button & create board button and boardheader component

--- src/Components/BoardComponent.jsx
+++ src/Components/BoardComponent.jsx
@@ -3,44 +3,54 @@ import AssignmentProvider from '../Providers/AssignmentProvider';
 import {BoardContext} from "../Providers/BoardProvider";
 import {AssignmentContext} from "../Providers/AssignmentProvider";
 import NavbarListComponent from './NavbarListComponent';
-import HeaderComponent from './HeaderComponent';
+//import HeaderComponent from './HeaderComponentOld';
 import ColumnComponent from './ColumnComponent';
 import ColumnHeaderComponent from './ColumnHeaderComponent';
 import NavbarComponent from "./NavbarComponent";
+import CreateAssignmentComponent from "./CreateAssignmentComponent";
+import HeaderBoardComponent from "./HeaderBoardComponent";
 
 function LoadAssignmentBoard() {
     const assignments = useContext(AssignmentContext);
 
     return (
-        <div data-testid="required-column-list" className={"board-div grid grid-cols-3 gap-4"}>
-            <ColumnComponent columnName={"🔵 To Do"}>
-                {assignments.filter(assignment => assignment.status === "TODO").map(assignment => <p>{assignment.name}</p>)}
-            </ColumnComponent>
-            <ColumnComponent columnName={"🔴 In Progress"}>
-                {assignments.filter(assignment => assignment.status === "In progress").map(assignment => <p>{assignment.name}</p>)}
-            </ColumnComponent>
-            <ColumnComponent columnName={"⚪ Done"}>
-                {assignments.filter(assignment => assignment.status === "Done").map(assignment => <p>{assignment.name}</p>)}
-            </ColumnComponent>
+        <div style={{marginLeft: "2vw"}}>
+            <div data-testid="required-column-list" className={"board-div grid grid-cols-3 gap-12 items-stretch"}>
+                <ColumnComponent columnName={"🔵 To Do"}>
+                    {assignments.filter(assignment => assignment.status === "TODO").map(assignment =>
+                        <p>{assignment.name}</p>)}
+                </ColumnComponent>
+                <ColumnComponent columnName={"🔴 In Progress"}>
+                    {assignments.filter(assignment => assignment.status === "In progress").map(assignment =>
+                        <p>{assignment.name}</p>)}
+                </ColumnComponent>
+                <ColumnComponent columnName={"⚪ Done"}>
+                    {assignments.filter(assignment => assignment.status === "Done").map(assignment =>
+                        <p>{assignment.name}</p>)}
+                </ColumnComponent>
+            </div>
         </div>
     );
 }
+
 export default function BoardComponent(props) {
 
     const boards = useContext(BoardContext);
     const [selectedBoard, setSelectedBoard] = useState(null);
-    if (boards.length > 0 && selectedBoard == null)
-    {
+    if (boards.length > 0 && selectedBoard == null) {
         setSelectedBoard(boards[0]);
     }
 
     return (
-        <div style={{display: "flex", alignItems: "start"}}>
+        <div style={{display: "flex"}}>
             {/*<HeaderComponent />*/}
             <NavbarComponent boards={boards} selectedBoard={selectedBoard} setSelectedBoard={setSelectedBoard}/>
             <AssignmentProvider board={selectedBoard}>
-                <LoadAssignmentBoard />
+                <LoadAssignmentBoard/>
             </AssignmentProvider>
+            <HeaderBoardComponent/>
+
+
         </div>
 
     )
--- src/Components/NavbarComponent.jsx
+++ src/Components/NavbarComponent.jsx
@@ -1,6 +1,7 @@
 import './css/NavbarComponent.css'
 import {useState} from "react";
 import NavbarListComponent from "./NavbarListComponent";
+import CreateBoardComponent from "./CreateBoardComponent";
 export default function NavbarComponent(props){
     const allTasks=[
         {name: 'To do', id: 1},
@@ -9,6 +10,10 @@ export default function NavbarComponent(props){
     ];
 
     return <div className={"navbar-container"}>
+        <div className={"boards-title"}>
+            Boards
+            <CreateBoardComponent/>
+        </div>
         <NavbarListComponent title={'Boards'} boards={props.boards} selectedBoard={props.selectedBoard} setSelectedBoard={props.setSelectedBoard}/>
         <NavbarListComponent title={'Tasks'} boards={allTasks}/>
     </div>
