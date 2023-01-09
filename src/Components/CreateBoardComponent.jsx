2bd14394495ffff95032458f8cc44a4aef5593d1
Noah Overeem <473544@student.fontys.nl>
2023-01-09 17:51:50 +0100

    Add and remove columns

--- src/Components/CreateBoardComponent.jsx
+++ src/Components/CreateBoardComponent.jsx
@@ -32,6 +32,8 @@ function CoursePill(props) {
     );
 }
 export default function CreateBoardComponent() {
+    const [columns, setColumns] = React.useState([]);
+    const [columnName, setColumnName] = React.useState("");
     const [showModal, setShowModal] = React.useState(false);
     const [selectedCards, setSelectedCards] = React.useState([]);
     const [board, setBoard] = useState({name: '', description: '', courses: []})
@@ -77,6 +79,15 @@ export default function CreateBoardComponent() {
         setSubmit(true);
     }
 
+    const createColumn = (columnname) => {
+        setColumns([...columns, {columnName: columnname}]);
+    }
+
+    const removeColumn = (columnname) => {
+        var c = columns
+        var filtered = c.filter(function(re) {return re.columnName !== columnname; })
+        setColumns(filtered);
+    }
 
     return (
         <>
@@ -94,7 +105,7 @@ export default function CreateBoardComponent() {
                         <div className="relative w-3/4 my-6 mx-auto max-w-3xl">
                             {/*content*/}
                             <div
-                                className="border-0 w-auto rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                                className="border-0 w-auto rounded-lg shadow-lg relative flex flex-col w-full bg-white mt-60 outline-none focus:outline-none">
                                 {/*header*/}
                                 <div
                                     className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
@@ -135,6 +146,34 @@ export default function CreateBoardComponent() {
                                             {courses.map((course) => <CoursePill course={course} selectedCards={selectedCards} setSelectedCards={setSelectedCards} handleRemoveItem={handleRemoveItem} />
                                             )}
                                         </div>
+                                        <div>
+                                            <div id={"allColumns"}>
+                                                <input type="text" name="columnID1Name" id="ColumnNameId"
+                                                       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                                                       placeholder="Column name" onChange={(e) => {setColumnName(e.target.value)}} required/>
+                                                <button
+                                                    className="text-black bg-slate-100 font-bold uppercase mb-5 px-6 py-3 rounded shadow hover:shadow-lg text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                                    type="button"
+                                                    onClick={() => createColumn(columnName)}>
+                                                    Create Column</button>
+                                                {columns.map((e) => {
+                                                    return (
+                                                        <div className={"flex inline my-4 mr-4"}>
+                                                            <button className={"flex inline text-black bg-slate-100 font-bold text-m mr-5 uppercase px-3 py-2 rounded shadow hover:shadow-lg text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 "}
+                                                                    onClick={() => removeColumn(e.columnName)}>X</button>
+                                                            <label htmlFor="column1Name"className="block mb-2 text-sm font-large text-black text-xl flex inline">{e.columnName}</label>
+                                                   {/* <input type="text" name="column1Name" id="id"
+                                                           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                                                           placeholder="Column 1 Name" value={e.columnName} required/>*/}
+
+                                                        </div>
+                                                    )
+                                                })}
+
+
+                                            </div>
+                                        </div>
+
                                         <div
                                             className="flex items-center justify-end pt-6 border-t border-solid border-slate-200 rounded-b">
                                             <button
